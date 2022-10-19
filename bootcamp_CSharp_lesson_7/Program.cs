da50f93a0313f195ee1b957787e8dcb9d74a1c51
ivan2798 <inge1998@hotmail.com>
2022-10-19 10:07:39 -0500

    POO V

--- bootcamp_CSharp_lesson_7/Program.cs
+++ bootcamp_CSharp_lesson_7/Program.cs
@@ -11,108 +11,18 @@ namespace bootcamp_CSharp_lesson_7
 
         static void Main(string[] args)
         {
-            Coche coche1 = new Coche(); // crear objeto/instancia de tipo Coche.
-            //dar un estado inical a nuestro coche
+            realizarTarea();
 
-            Coche coche2 = new Coche();
+        }
 
-            Console.WriteLine(coche1.getInfoCoche());
-            Console.WriteLine(coche2.getInfoCoche());
+        static void realizarTarea()
+        {
+            Punto origen = new Punto();
 
-            Coche coche3 = new Coche(4500.25, 1200.35);
-
-            Console.WriteLine(coche3.getInfoCoche());
-
-            coche3.setExtras(true, "cuero");
-
-            Console.WriteLine(coche3.getExtras());
-
+            Punto destino = new Punto(128, 80);
         }
 
 
     }
 
-    partial class Coche
-    {
-        public Coche()
-        {
-            ruedas = 4;
-
-            largo = 2300.5;
-
-            ancho = 0.800;
-
-            tapiceria = "tela";
-
-        }
-
-        public Coche(double largo, double ancho)
-        {
-            this.ruedas = 4;
-            this.largo = largo;
-            this.ancho = ancho;
-            this.tapiceria = "tela";
-        }
-    }
-
-    partial class Coche
-    { 
-
-        public string getInfoCoche()
-        {
-            return $"Informacion del coche: Ruedas: {ruedas}, Largo: {largo}, Ancho:{ancho} ";
-        }
-
-        public void setExtras(bool climatizador, string tapiceria)
-        {
-            this.climatizador = climatizador;
-
-            this.tapiceria = tapiceria;
-        } 
-
-        public string getExtras()
-        {
-            return $"Extras del coche: Tapicería {tapiceria}, Climatizador {climatizador}";
-        }
-
-        private int ruedas;
-
-        private double largo;
-
-        private double ancho;
-
-        private bool climatizador;
-
-        private String tapiceria;
-    }
-    class Circulo
-    {
-
-        private const double PI = 3.1216;  //propiedad de la clase Círculo. Campo de clase.
-
-        public double CalculoArea(int radio) // método de cla. ¿Qué pueden hacer los objetos de tipo círculo?
-        {
-
-            return PI * radio * radio;
-        }
-     
-    }
-
-    class ConversorEuroDolar
-    {
-        private double euro = 1.253;
-
-        public double convierte(double cantidad)
-        {
-            return cantidad * euro;
-        }
-
-        public void cambiValorEuro(double nuevoValor)
-        {
-            if (nuevoValor < 0) euro = 1.253;
-
-            else
-                euro = nuevoValor;
-        }
-    }
 }
---
+++ bootcamp_CSharp_lesson_7/Punto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bootcamp_CSharp_lesson_7
+{
+    class Punto
+    {
+        public Punto(int x, int y)
+        {
+            Console.WriteLine($"Coordenada x: {x}, Coordenada y: {y}");
+        }
+
+        public Punto()
+        {
+            Console.WriteLine($"Constructor por defecto");
+        }
+
+    }
+}
